87a702ce1ae3b2458d88e0dd360f09cc0c04a55d
wawazoe <ngycxzbsnbf508j08gpb@docomo.ne.jp>
2022-06-10 18:26:26 +0900

    issue4~6の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -42,7 +42,7 @@ private
         wday_num = wday_num -7
       end 
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wdays => wdays[wday_num] } 
+      days = { month: (@todays_date + x).month, date:  (@todays_date + x).day, plans:  today_plans, wdays:  wdays[wday_num] } 
       @week_days.push(days)
     end
   end
